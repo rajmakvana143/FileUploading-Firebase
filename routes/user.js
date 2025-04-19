ffbe4c7396e2d01f4cf440de8b9eb669e9aa86ab
rajmakvana143 <rajmakvana143e@gmail.com>
2025-04-19 15:11:47 +0530

    save password in hashing formate

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { query , body , validationResult } = require('express-validator');
 const  User =  require('../model/user_model');
+const bcrypt = require('bcrypt');
 
 
 router.get('/' , (req , res) => {
@@ -16,14 +17,26 @@ router.post('/register' ,
     body('username').trim(),
     body('email').trim().isEmail().isLength({min : 5}),
     body('password').trim().isLength({min : 5}).withMessage('Password must be at least 5 characters long'),
-    (req , res) => {
+    async (req , res) => {
 
     const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
 
-    res.send('User Registered Successfully!');
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username , email , password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+        username : username,
+        email : email,
+        password : hashedPassword,
+    });
+
+    res.json(newUser)
+
+    // res.send('User Registered Successfully!');
 });
 
 
