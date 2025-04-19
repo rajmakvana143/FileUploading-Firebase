7794e4600260d7382592a2a173502c71970dea45
rajmakvana143 <rajmakvana143e@gmail.com>
2025-04-19 13:00:07 +0530

    user model created

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
 const PORT = 3000;
  
 const userRouter = require('./routes/user');
+const connectDB = require('./config/dataBase');
+connectDB();
 
 
 app.set('view engine', 'ejs');
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { query , body , validationResult } = require('express-validator');
+const  User =  require('../model/user_model');
 
 
 router.get('/' , (req , res) => {
